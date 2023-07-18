3f2b5164ce78040fd507aee2a07e55212cb01b78
Vontell <sankarsh.rajeev@gmail.com>
2023-07-18 11:51:44 -0700

    ui complete

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TranscationList } from './components/TranscationList';
-
+import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
         <IncomeExpenses />
         <TranscationList />
+        <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   ); 
 }
 
